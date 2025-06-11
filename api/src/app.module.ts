5508c633f65474b3d9e5e62be0d94c3001e6f50a
OSCAR RAUL GUALDRON VILLAMIZAR <97969534+OscarRGualdronV@users.noreply.github.com>
2025-06-11 12:41:57 -0500

    Implementacion de Cloudinary y creación de modulos usuarios y organizaciones
    1. Se creo un modulo para la conexión a cloudinary contiene:
       * provider
       * service
       *module
       * controller
    2. se creo una ruta en el controlador del modulo de cloudinary para probar que los archivos se suben correctamente.
    3. Se crearon dos módulos usuarios y organizaciones.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { ConfigModule } from '@nestjs/config';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { OrganizacionesModule } from './organizaciones/organizaciones.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+    }),
+    CloudinaryModule,
+    UsuariosModule,
+    OrganizacionesModule],
   controllers: [AppController],
   providers: [AppService],
 })
