b72468f8d7af58dbaf467342f345c47d992bea86
OSCAR RAUL GUALDRON VILLAMIZAR <97969534+OscarRGualdronV@users.noreply.github.com>
2025-06-11 17:20:12 -0500

    Creación de metodos para actualizar imagen de perfil.
    1. Se crea el método actualizarFotoPerfil en el servicio de usuarios y organizaciones.
    2. Se crean los correspondientes métodos en los controladores de usuarios y organizaciones para actualizar la imagen de perfil POST(':id/foto').

--- api/src/usuarios/usuarios.module.ts
+++ api/src/usuarios/usuarios.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { PrismaModule } from 'prisma/prisma.module';
+import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
 
 @Module({
+  imports: [PrismaModule, CloudinaryModule],
   controllers: [UsuariosController],
   providers: [UsuariosService],
 })
--- api/src/usuarios/usuarios.service.ts
+++ api/src/usuarios/usuarios.service.ts
@@ -1,26 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUsuarioDto } from './dto/create-usuario.dto';
-import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { PrismaService } from 'prisma/prisma.service';
+
 
 @Injectable()
 export class UsuariosService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
-  }
+  constructor( private readonly prisma: PrismaService){}
 
-  findAll() {
-    return `This action returns all usuarios`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} usuario`;
-  }
-
-  update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
-    return `This action updates a #${id} usuario`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} usuario`;
+  async actualizarFotoPerfil(id: string, fotoUrl: string){
+    return this.prisma.usuario.update({
+      where: { id },
+      data: { imagenPerfil: fotoUrl}
+    });
   }
 }
