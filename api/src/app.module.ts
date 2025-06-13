d983d008b88dfd09e4805688f14f8444cd2ece55
Samuel <samuelfonseca01003@gmail.com>
2025-06-13 09:48:11 -0500

    Registro de usuarios con auth0

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config'
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { OrganizacionesModule } from './organizaciones/organizaciones.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaModule } from 'src/prisma/prisma.module'
+
 
 @Module({
   imports: [
