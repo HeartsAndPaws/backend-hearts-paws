e6476ca3285b362285d57f16b65eb60a68ed1364
OSCAR RAUL GUALDRON VILLAMIZAR <97969534+OscarRGualdronV@users.noreply.github.com>
2025-06-18 09:28:14 -0500

    Implementación de Swagger.

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -4,14 +4,4 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('ping')
-  getPing(){
-    return {message: 'Backend activo y corriendo'}
-  }
 }
