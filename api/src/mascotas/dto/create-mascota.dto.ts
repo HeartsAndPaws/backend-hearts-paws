394aec46eaab5f8812a993518d63e208f99f8244
Samuel <samuelfonseca01003@gmail.com>
2025-06-19 07:28:28 -0500

    Crud, registro y subida de fotos de mascotas. Cambios en prisma.schema

--- api/src/mascotas/dto/create-mascota.dto.ts
+++ api/src/mascotas/dto/create-mascota.dto.ts
@@ -1,1 +1,23 @@
-export class CreateMascotaDto {}
+import { IsString, IsNotEmpty, IsOptional, IsNumber, Min, IsUUID } from 'class-validator';
+
+export class CreateMascotaDto {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsNumber()
+  @Min(0)
+  edad: number;
+
+  @IsString()
+  @IsOptional()
+  descripcion?: string;
+
+  @IsUUID()
+  @IsNotEmpty()
+  organizacionId: string;
+
+  @IsUUID()
+  @IsNotEmpty()
+  tipoId: string;
+}
--- api/src/mascotas/mascotas.controller.ts
+++ api/src/mascotas/mascotas.controller.ts
@@ -1,14 +1,61 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFiles } from '@nestjs/common';
 import { MascotasService } from './mascotas.service';
 import { CreateMascotaDto } from './dto/create-mascota.dto';
 import { UpdateMascotaDto } from './dto/update-mascota.dto';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
 @Controller('mascotas')
 export class MascotasController {
   constructor(private readonly mascotasService: MascotasService) {}
 
   
+    @Get()
+    GetMascotas(){
 
+      return this.mascotasService.GetMascotas();
+
+    }
+
+    @Get('tipo')
+    GetTiposMascotas() {
+      return this.mascotasService.GetTipo();
+    }
+
+    @Get(':id')
+    GetMascotaById(@Param('id') id: string) {
+      return this.mascotasService.GetMascotaById(id);
+    }
+
+
+    @Post()
+    CreateMascota(@Body() createMascotaDto: CreateMascotaDto) {
+      return this.mascotasService.CreateMascota(createMascotaDto);
+    }
+
+    @Post(':id/imagenes')
+    @UseInterceptors(FilesInterceptor('imagenes'))
+    @ApiConsumes('multipart/form-data')
+    @ApiBody({
+  schema: {
+    type: 'object',
+    properties: {
+      imagenes: {
+        type: 'array',
+        items: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  },
+})
+    async subirImagenes(
+      @Param('id') mascotaId: string,
+      @UploadedFiles() archivos: Express.Multer.File[],
+    ) {
+      return this.mascotasService.SubirImagenes(mascotaId, archivos);
+    }
 
 
 }
