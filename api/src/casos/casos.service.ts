53e9b62141e1a8bb133e98d9e42371169d484779
Samuel <samuelfonseca01003@gmail.com>
2025-06-19 22:26:07 -0500

    Crud, registro y subida de fotos de mascotas. Cambios en prisma.schema

---
+++ api/src/casos/casos.service.ts
@@ -0,0 +1,134 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateCasoDto } from './dto/create-caso.dto';
+import { UpdateCasoDto } from './dto/update-caso.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CasosService {
+  constructor(private readonly prismaService: PrismaService){}
+
+
+  async GetCasos() {
+
+    return this.prismaService.caso.findMany({
+      include: {
+        mascota: true,
+        ong: true,
+      }
+    });
+
+  }
+
+  
+  
+  async GetCasoById(id: string) {
+    return this.prismaService.caso.findUnique({
+      where: { id },
+      include: {
+        mascota: true,
+        ong: true,
+      },
+    });
+  }
+  
+  async GetCasosAdopcion() {
+    return this.prismaService.caso.findMany({
+      where: { tipo: 'ADOPCION' },
+      include: {
+        mascota: true,
+        ong: true,
+      },
+    });
+    
+  }
+
+  async GetCasosDonacion() {
+    return this.prismaService.caso.findMany({
+      where: { tipo: 'DONACION' },
+      include: {
+        mascota: true,
+        ong: true,
+      },
+    });
+    
+  }
+
+  async CreateCaso(createCasoDto: CreateCasoDto) {
+    
+    const casoExistente = await this.prismaService.caso.findFirst({
+      where: {
+        tipo: createCasoDto.tipo,
+        mascotaId: createCasoDto.mascotaId,
+      }
+    })
+
+    if (casoExistente) {
+      throw new BadRequestException(`Ya existe un caso de tipo ${createCasoDto.tipo} para la mascota con ID ${createCasoDto.mascotaId}`);
+    }
+
+     const caso = await this.prismaService.caso.create({
+      data: {
+        titulo: createCasoDto.titulo,
+        descripcion: createCasoDto.descripcion,
+        tipo: createCasoDto.tipo,
+        mascotaId: createCasoDto.mascotaId,
+        ongId: createCasoDto.ongId,
+      },
+    });
+
+    if(createCasoDto.tipo === 'ADOPCION') {
+
+      await this.prismaService.casoAdopcion.create({
+        data: {
+          casoId: caso.id,
+          estado: 'PENDIENTE',
+        },
+      });
+
+    } else if(createCasoDto.tipo === 'DONACION' && createCasoDto.donacion) {
+      await this.prismaService.casoDonacion.create({
+        data: {
+          casoId: caso.id,
+          metaDonacion: createCasoDto.donacion.metaDonacion,
+          estadoDonacion: 0,
+        },
+      });
+    }
+
+    return caso;
+
+  }
+
+  async buscarCasos(filtros: { tipoMascota?: string; nombreMascota?: string }) {
+    
+    return this.prismaService.caso.findMany({
+      where: {
+        mascota: {
+          ...(filtros.tipoMascota && {
+            tipo: { nombre: { equals: filtros.tipoMascota, mode: 'insensitive' } }
+          }),
+          ...(filtros.nombreMascota && {
+            nombre: { contains: filtros.nombreMascota, mode: 'insensitive' }
+          })
+        }
+      },
+      include: {
+        mascota: {
+          include: {
+            tipo: true,
+            imagenes: true,
+          }
+        },
+        ong: true,
+        adopcion: true,
+        donacion: true,
+      }
+     
+
+    })
+
+  }
+
+  
+
+}
