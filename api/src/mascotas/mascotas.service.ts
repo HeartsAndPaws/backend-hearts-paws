a0ef627da16be53c3ff81b77a30201651909758d
OSCAR RAUL GUALDRON VILLAMIZAR <97969534+OscarRGualdronV@users.noreply.github.com>
2025-07-05 11:06:06 -0500

    Integracion de Blur en imagenes con violencia
    1. Se modifico la ruta de crear solicitudes, para integrar un usuario de supabase.
    2. Se implemento lógica para hacer Blur a las fotos con contenido violento o de sangre.
    3. Se aplicaron guardas a las rutas usadas por la Ong.

--- api/src/mascotas/mascotas.service.ts
+++ api/src/mascotas/mascotas.service.ts
@@ -3,6 +3,7 @@ import { CreateMascotaDto } from './dto/create-mascota.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CloudinaryService } from '../cloudinary/cloudinary.service';
 import { TipoMascotaDto } from './dto/tipoMascota.dto';
+import axios from 'axios';
 
 @Injectable()
 export class MascotasService {
@@ -104,14 +105,38 @@ async CreateMascota(createMascotaDto: CreateMascotaDto) {
     const imagenes: any = []
 
     for(const file of archivos){
-
       const result = await this.cloudinaryService.subirIamgen(file);
+
+      const res = await axios.get('https://api.sightengine.com/1.0/check.json', {
+        params: {
+          url: result.secure_url,
+          models: 'violence, gore',
+          api_user: process.env.SIGHTENGINE_USER,
+          api_secret: process.env.SIGHTENGINE_SECRET,
+        },
+      });
+
+      const analisis = res.data;
+      const violenciaScore = Math.max(
+        res.data.violence?.prob || 0,
+        res.data.gore?.prob || 0
+      );
+      const esSensible = violenciaScore > 0.7;
+
+      let urlDesenfocada: string | null = null;
+      if (esSensible) {
+        const urlOriginal = result.secure_url;
+        const partes = urlOriginal.split('/upload/');
+        urlDesenfocada = `${partes[0]}/upload/e_pixelate:100/${partes[1]}`;
+      }
+
       const imagen = await this.prismaService.imagenMascota.create({
         data: {
-
           url: result.secure_url,
+          urlBlur: urlDesenfocada,
           mascotaId,
-          
+          violenciaScore,
+          esSensible,
         },
       });
 
