c68a5f3d37e12e5670bef4ac11c08a560c870a73
OSCAR RAUL GUALDRON VILLAMIZAR <97969534+OscarRGualdronV@users.noreply.github.com>
2025-07-09 18:59:19 -0500

    Update ActualizarUsuario.dto.ts

--- api/src/usuarios/dto/ActualizarUsuario.dto.ts
+++ api/src/usuarios/dto/ActualizarUsuario.dto.ts
@@ -1,4 +1,14 @@
-import { IsEmail, IsString, IsStrongPassword, IsNotEmpty, MinLength, MaxLength, Matches, IsOptional, IsEnum } from "class-validator";
+import {
+  IsEmail,
+  IsString,
+  IsStrongPassword,
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  Matches,
+  IsOptional,
+  IsEnum
+} from "class-validator";
 import { Transform } from "class-transformer";
 const xss = require('xss');
 import { Trim } from "src/autenticacion/decoradores/trim.decorator";
@@ -6,94 +16,90 @@ import { ApiPropertyOptional } from "@nestjs/swagger";
 import { Rol } from "@prisma/client";
 
 export class ActualizarUsuarioDTO {
-    @IsOptional()
-    @ApiPropertyOptional({
+  @IsOptional()
+  @ApiPropertyOptional({
     example: 'usuario123@email.com',
-    description: 'Correo electrónico válido. Solo se permite un "@" y letras, números, ".", "_" o "-".',
+    description: 'Correo electrónico válido.',
     maxLength: 60,
-    })
-    @IsEmail()
-    @Matches(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+$/, {
-    message: 'Email: solo se permite un "@" y letras, números, ".", "_" o "-"',})
-    @Trim()
-    @Transform(({ value }) => value.toLowerCase())
-    @MaxLength(60)
-    email?: string;
+  })
+  @IsEmail()
+  @Trim()
+  @Transform(({ value }) => value.toLowerCase())
+  @MaxLength(60)
+  email?: string;
 
-    @IsOptional()
-    @ApiPropertyOptional({
+  @IsOptional()
+  @ApiPropertyOptional({
     example: 'ClaveSegura#2024',
-    description:
-    'Contraseña segura que debe tener al menos 1 letra mayúscula, 1 minúscula, 1 número y 1 símbolo.',
+    description: 'Contraseña con al menos 1 mayúscula, 1 minúscula, 1 número y 1 símbolo.',
     minLength: 8,
     maxLength: 40,
-    })
-    @IsString()
-    @Transform(({ value }) => xss(value))
-    @MinLength(8)
-    @MaxLength(40)
-        @IsStrongPassword({
-        minUppercase:1,
-        minLowercase:1,
-        minNumbers:1,
-        minSymbols:1
-    })
-    contrasena?: string;
+  })
+  @IsString()
+  @Transform(({ value }) => xss(value))
+  @MinLength(8)
+  @MaxLength(40)
+  @IsStrongPassword({
+    minUppercase: 1,
+    minLowercase: 1,
+    minNumbers: 1,
+    minSymbols: 1
+  })
+  contrasena?: string;
 
-    @IsOptional()
-    @ApiPropertyOptional({
+  @IsOptional()
+  @ApiPropertyOptional({
     example: '3001234567',
-    description: 'Número de teléfono del usuario. Solo debe contener dígitos.',
+    description: 'Número de teléfono del usuario.',
     maxLength: 20,
-    })
-    @IsString()
-    @Transform(({ value }) => xss(value))
-    @Trim()
-    @Transform(({ value }) => value.replace(/\D/g, ''))
-    @MaxLength(20)
-    telefono?: string;
+  })
+  @IsString()
+  @Transform(({ value }) => xss(value))
+  @Trim()
+  @Transform(({ value }) => value.replace(/\D/g, ''))
+  @MaxLength(20)
+  telefono?: string;
 
-    @IsOptional()
-    @ApiPropertyOptional({
+  @IsOptional()
+  @ApiPropertyOptional({
     example: 'Calle 45 #67-89, Apto 302',
-    description:
-    'Dirección física del usuario. Solo se permiten letras, números, espacios, puntos, comas, guiones y guiones bajos.',
+    description: 'Dirección física del usuario.',
     maxLength: 70,
-    })
-    @IsString()
-    @Matches(/^[a-zA-Z0-9.,_\-\s]+$/, {
-    message: 'Dirección: solo se permiten letras, números, espacios, puntos, comas, guiones medios, guiones bajos.',
-})
-    @MaxLength(70)
-    direccion?: string;
+  })
+  @IsString()
+  @Transform(({ value }) => xss(value))
+  @MaxLength(70)
+  direccion?: string;
 
-    @IsOptional()
-    @ApiPropertyOptional({
-    example: 'Medellín',
-    description: 'Ciudad donde reside el usuario. Solo se permiten letras, números y espacios.',
+  @IsOptional()
+  @ApiPropertyOptional({
+    example: 'Bogotá',
+    description: 'Ciudad donde reside el usuario.',
     maxLength: 25,
-    })
-    @IsString()
-    @Matches(/^[A-Za-z0-9 ]+$/, {
-    message: 'Ciudad:Solo se permiten letras, números y espacios',
-})
-    @MaxLength(25)
-    ciudad?: string;
+  })
+  @IsString()
+  @Transform(({ value }) => xss(value))
+  @Matches(/^[\p{L}0-9 .,'-]+$/u, {
+    message: 'Ciudad: Solo se permiten letras, números y algunos caracteres como . , \' -',
+  })
+  @MaxLength(25)
+  ciudad?: string;
 
-    @IsOptional()
-    @ApiPropertyOptional({
+  @IsOptional()
+  @ApiPropertyOptional({
     example: 'Colombia',
-    description: 'País de residencia. Solo letras y números, sin espacios.',
+    description: 'País de residencia.',
     maxLength: 25,
-    })
-    @MaxLength(25)
-    @IsString()
-    @Matches(/^[a-zA-Z0-9]+$/, {
-    message: 'Pais: solo se permiten letras y números',
-})
-    pais?: string;
+  })
+  @IsString()
+  @Transform(({ value }) => xss(value))
+  @Matches(/^[\p{L}0-9 ]+$/u, {
+    message: 'País: Solo se permiten letras, números y espacios',
+  })
+  @MaxLength(25)
+  pais?: string;
 
-    @IsOptional()
-    @IsEnum(Rol)
-    rol?: Rol;
+  @IsOptional()
+  @IsEnum(Rol)
+  rol?: Rol;
 }
